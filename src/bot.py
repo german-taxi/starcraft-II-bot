0225631cc24f57e39b771bfa9de401137854bf94
arnasvid <arnas.vidziunas@gmail.com>
2022-06-03 23:11:41 +0300

    adding function to scout enemy's base

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,11 @@
-from sc2 import maps
+from sc2 import maps, position
 from sc2.player import Bot, Computer
 from sc2.main import run_game
 from sc2.data import Race, Difficulty
 from sc2.bot_ai import BotAI
 from sc2.ids.unit_typeid import UnitTypeId
 import sys
+import random
 
 from sc2.unit import Unit
 
@@ -16,6 +17,7 @@ from HeatMaps.defender_area_map import DefendedAreaMap
 from Managers.army_manager import ArmyManager
 from Managers.worker_manager import WorkerManager
 from Helper.time_to_travel_map import TimeToTravel
+from Managers.scouting_manager import ScoutingManager
 
 print(sys.version)
 
@@ -37,6 +39,7 @@ class WorkerRushBot(BotAI):
         self.fast_iteration_speed = 1
         self.medium_iteration_speed = 3
         self.slow_iteration_speed = 15
+        self.s_managers = []
 
     def spend_resources(self):
         if self.can_afford(self.next_item.unit_type):
@@ -76,6 +79,7 @@ class WorkerRushBot(BotAI):
         self.unit_by_tag = {unit.tag: unit for unit in self.all_units}  # why doesn't work with all_my_units?
         self.w_managers.append(WorkerManager(self, self.townhalls[0]))
         self.a_managers.append(ArmyManager(self))
+        self.s_managers.append(ScoutingManager(self))
 
 
         self.danger_map = DangerMap(self, [640, 640])
@@ -83,18 +87,20 @@ class WorkerRushBot(BotAI):
         # self.time_to_travel = TimeToTravel(self)
 
     async def on_step(self, iteration: int):
+  
         self.unit_by_tag = {unit.tag: unit for unit in self.all_units}
 
         if (iteration % self.fast_iteration_speed == 0):
             for w_manager in self.w_managers:
                 w_manager.update()
-
+            await self.s_managers[0].scout()   
+        
         if (iteration % self.medium_iteration_speed == 0):
             pass
 
         if (iteration % self.slow_iteration_speed == 0):
             pass
-
+                  
 
 if __name__ == "__main__":
     run_game(maps.get(map_name), [
