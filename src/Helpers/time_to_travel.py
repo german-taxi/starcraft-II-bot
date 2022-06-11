e579f4ad56bc2ede39a87925a137a7bc4336c305
KerniusSur <kernius.survila@gmail.com>
2022-06-11 19:09:57 +0300

    revert the changes made by deepsource

--- src/Helpers/time_to_travel.py
+++ src/Helpers/time_to_travel.py
@@ -2,13 +2,8 @@ class TimeToTravel:
     def __init__(self):
         pass
 
-<<<<<<< HEAD
     # TODO: Calculate the time it takes to walk somewhere
     def get_time_to_travel(self):
-=======
-    @staticmethod
-    def get_time_to_travel(unit_type, starting_location, destination):
->>>>>>> 0895e930b7488498855a235a65f73985fa1c066e
         """
         Returns the time it takes to travel from the starting location to the destination, in seconds.
         """
